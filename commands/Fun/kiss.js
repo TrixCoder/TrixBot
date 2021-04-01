2176c334d7b40f5603ff81a2b0e5c69b23415ce3
Utkarsh Kumar <utk.bkkg@gmail.com>
2021-04-01 12:44:38 +0530

    Added some fun commands

---
+++ commands/Fun/kiss.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require('discord.js');
+const axios = require('axios');
+const config = require('./../../config.js')
+
+module.exports.run = async (client, msg, args) => {
+    let user = msg.guild.members.cache.get(args[0]) || msg.mentions.users.first() || msg.author;
+
+    let res = await axios.get(`https://api.tenor.com/v1/search?q=anime kiss&key=${config.TENOR_API}&contentfilter=medium`);
+
+    const filter = res.data.results.filter(r => r.url.endsWith('.gif'));
+
+    const gif = filter[Math.floor(Math.random() * filter.length)];
+
+    let embed = new MessageEmbed()
+        .setDescription(`${user.id === msg.author.id ? `**${msg.author.toString()} wants some kisses.**` : `**${msg.author.toString()} kissed ${user.toString()}**`}`)
+        .setImage(gif.media[0].gif.url)
+        .setTimestamp()
+        .setColor("RANDOM")
+    msg.channel.send(embed);
+}
+
+
+module.exports.help = {
+    name: 'kiss',
+    aliases: ['']
+}
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   prefix: "t",
   token: "Nzg3OTgwNjAzODAyMjU1MzYy.X9c2Hw.ZWJvY4JYNpefiY9Lzq9srHvgzSo",
+  TENOR_API: 'QBLAQ673GIDZ',
 }
