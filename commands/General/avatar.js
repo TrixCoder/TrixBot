94e86c4fbea0176d173a345b8961657e54f4e432
Utkarsh Kumar <utk.bkkg@gmail.com>
2021-04-01 18:39:20 +0530

    Added avatar command

---
+++ commands/General/avatar.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports.run = async (client,message, args) => {
+    let user;
+    if (!args[0]) {
+        user = message.guild.members.cache.get(message.author.id).user;
+    } else {
+        user = message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase().startsWith(args.join(' ').toLowerCase()));
+    }
+    let embed = new MessageEmbed()
+        .setTitle(`**${user.tag}'s avatar**`)
+        .setDescription(`**Link as** \n [png](${user.avatarURL({ format: 'png', size: 4096 })}) - [jpg](${user.avatarURL({ format: 'jpg', size: 4096 })}) - [webp](${user.avatarURL({ format: 'webp', size: 4096 })})`)
+        .setImage(user.avatarURL({ format: 'png', dynamic: true, size: 4096 }))
+        .setColor('RANDOM')
+
+    return message.channel.send(embed);
+}
+
+module.exports.help = {
+    name: 'avatar',
+    aliases: ['av']
+}
