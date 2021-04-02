9d8b3dd27bfb6ab2fe7153c17daefb0cda07b35b
Utkarsh Kumar <utk.bkkg@gmail.com>
2021-04-02 12:27:01 +0530

    Updated token

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  prefix: "t",
-  token: "Nzg3OTgwNjAzODAyMjU1MzYy.X9c2Hw.ZWJvY4JYNpefiY9Lzq9srHvgzSo",
+  prefix: "t!",
+  token: "Nzg3OTgwNjAzODAyMjU1MzYy.X9c2Hw.v8Arnw62vPwZixtb447_FB7JGcM",
   TENOR_API: 'QBLAQ673GIDZ',
 }
