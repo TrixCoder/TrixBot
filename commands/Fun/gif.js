0e0406aa1f9f6703bf5a37cf15f21b0c025b78ed
Utkarsh Kumar <utk.bkkg@gmail.com>
2021-04-06 09:08:21 +0530

    Fixed gif command

--- commands/Fun/gif.js
+++ commands/Fun/gif.js
@@ -5,14 +5,24 @@ const config = require('./../../config.js')
 module.exports.run = async (client, msg, args) => {
     if (!args[0]) return msg.channel.send(`Please provide a valid gif name to search`);
 
-    let res = await axios.get(`https://api.tenor.com/v1/search?q=anime ${args[0]}&key=${config.TENOR_API}&contentfilter=medium`);
+    let content = args.slice(0);
+    let m = "";
+    for (let i = 0; i < content.length; i++) {
+        if (i == content.length - 1) {
+            m += `${content[i]}`;
+        } else {
+            m += `${content[i]} `;
+        }
+    }
+
+    let res = await axios.get(`https://api.tenor.com/v1/search?q=anime ${m}&key=${config.TENOR_API}&contentfilter=medium`);
 
     const filter = res.data.results.filter(r => r.url.endsWith('.gif'));
 
     const gif = filter[Math.floor(Math.random() * filter.length)];
 
     let embed = new MessageEmbed()
-        .setDescription(`**Searched ${args[0]}**`)
+        .setDescription(`**Searched ${m}**`)
         .setImage(gif.media[0].gif.url)
         .setTimestamp()
         .setFooter(`Requested by: ${msg.author.tag}`, msg.author.avatarURL({ dynamic: true, format: 'png', size: 4096 }))
